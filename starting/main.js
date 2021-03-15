760ccdc7015a0003a8b6d10bf4dcbc11d0fbedf6
ENEMIE <hanfihamza20@.gmail.com>
2021-03-15 16:08:39 +0100

    print method added

--- starting/main.js
+++ starting/main.js
@@ -1,6 +1,26 @@
 const prompt = require('prompt-sync')({sigint: true});
 
+
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+class Field {
+    constructor(arr){
+        this.field = arr ; 
+    }
+    print(){
+        let arr1 = this.field.map(e => e.join(''))
+        let result = arr1.join('\n')
+        return result ; 
+        };
+    navigate(){
+        
+    }
+}
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+ console.log(myField.print()) 
